5936e714b03892be57eeac7890fe003cebff8da3
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-14 14:04:40 -0400

    add class aliases

---
+++ init.php
@@ -0,0 +1,7 @@
+<?php
+
+class_alias(Winter\Debugbar\Plugin::class, RainLab\Debugbar\Plugin::class);
+
+class_alias(Winter\Debugbar\Classes\ServiceProvider::class, RainLab\Debugbar\Classes\ServiceProvider::class);
+
+class_alias(Winter\Debugbar\Middleware\InjectDebugbar::class, RainLab\Debugbar\Middleware\InjectDebugbar::class);
