80afca0240628d15a9fa84eefb9c6b068144ad18
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-15 09:52:27 -0400

    prevent creating the class aliases when the original plugin is still installed

--- init.php
+++ init.php
@@ -1,7 +1,9 @@
 <?php
 
-class_alias(Winter\Debugbar\Plugin::class, RainLab\Debugbar\Plugin::class);
+if (!class_exists(RainLab\Debugbar\Plugin::class)) {
+    class_alias(Winter\Debugbar\Plugin::class, RainLab\Debugbar\Plugin::class);
 
-class_alias(Winter\Debugbar\Classes\ServiceProvider::class, RainLab\Debugbar\Classes\ServiceProvider::class);
+    class_alias(Winter\Debugbar\Classes\ServiceProvider::class, RainLab\Debugbar\Classes\ServiceProvider::class);
 
-class_alias(Winter\Debugbar\Middleware\InjectDebugbar::class, RainLab\Debugbar\Middleware\InjectDebugbar::class);
+    class_alias(Winter\Debugbar\Middleware\InjectDebugbar::class, RainLab\Debugbar\Middleware\InjectDebugbar::class);
+}
