98e11677a3409efd1342e4c2ef30d750bf242818
Web-VPF <pavrub@yandex.ru>
2021-12-31 17:53:00 +0200

    Added Russian language

---
+++ lang/ru/lang.php
@@ -0,0 +1,8 @@
+<?php return [
+    'plugin' => [
+        'name'                   => 'Панель отладки',
+        'description'            => 'Интегрирует отладочную панель PHP с Winter CMS',
+        'access_debugbar'        => 'Доступ к панели отладки',
+        'access_stored_requests' => 'Доступ к сохраненным запросам (рисковано)',
+    ],
+];
