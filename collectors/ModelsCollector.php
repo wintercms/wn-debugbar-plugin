e63f0d0953d03e544a41f31690ac84a403ad88ed
systemsolutionweb <88460896+systemsolutionweb@users.noreply.github.com>
2024-02-15 10:58:22 -0500

    Support Debugbar ObjectCountCollector

--- collectors/ModelsCollector.php
+++ collectors/ModelsCollector.php
@@ -1,24 +1,17 @@
 <?php namespace Winter\Debugbar\Collectors;
 
-use Barryvdh\Debugbar\DataCollector\ModelsCollector as BaseModelsCollector;
-use Illuminate\Contracts\Events\Dispatcher;
+use DebugBar\DataCollector\ObjectCountCollector;
 use Winter\Storm\Database\Model;
 
-class ModelsCollector extends BaseModelsCollector
+class ModelsCollector extends ObjectCountCollector
 {
-    public $models = [];
-    public $count = 0;
-
-    /**
-     * @param Dispatcher $events
-     */
-    public function __construct(Dispatcher $events)
+    public function __construct()
     {
+        parent::__construct('models');
+
         Model::extend(function ($model) {
             $model->bindEvent('model.afterFetch', function () use ($model) {
-                $class = get_class($model);
-                $this->models[$class] = ($this->models[$class] ?? 0) + 1;
-                $this->count++;
+                $this->countClass($model);
             });
         });
     }
